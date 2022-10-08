c999df9abd3c30b24125ce107c45c490d09077e9
= <=>
2022-10-08 23:59:42 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './page/page.component';
-import { HelloComponent } from './hello/hello.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 @NgModule({
   declarations: [
